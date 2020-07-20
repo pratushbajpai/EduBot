354f34fb00905c51cd86566d2bcb94ad86b48e7c
Madhav Jakkampudi <madhav.jakkampudi@microsoft.com>
2020-07-19 17:12:23 -0700

    chapter chnages

--- QuestionsAdmin/QuestionsAdmin/Controllers/BotController.cs
+++ QuestionsAdmin/QuestionsAdmin/Controllers/BotController.cs
@@ -6,6 +6,7 @@ using QuestionsAdmin.Models;
 using QuestionsAdmin.Repository;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Security.AccessControl;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -19,14 +20,32 @@ namespace QuestionsAdmin.Controllers
         {
             this.botRepo = botRepo;
         }
+
         // GET: api/<BotController>
+        [Route("[action]/{chapter}")]
         [HttpGet]
-        public Questions Get()
+        public Questions GetByChapter(int chapter)
         {
-            return this.botRepo.GetQuestions();
+            if(chapter == 0)
+            {
+                return this.botRepo.GetQuestions();
+            }
+
+            List < Question > questionList = this.botRepo.GetQuestions().QuestionsList.Where(a => a.Chapter == chapter).ToList();
+            var questions = new Questions();
+            questions.QuestionsList = questionList;
+            return questions;
         }
 
-        
+        [Route("[action]")]
+        [HttpGet]
+        public List<string> AvailableChapters()
+        {
+            var availableChapters = this.botRepo.GetQuestions().QuestionsList.Select(x => "Chapter " + x.Chapter).Distinct().ToList();
+            availableChapters.Insert(0, "All chapters");
+            return availableChapters;
+        }
+
         // PUT api/<BotController>
         [HttpPut()]
         public void Put([FromBody] Questions value)
